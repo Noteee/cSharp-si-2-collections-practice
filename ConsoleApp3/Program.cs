67aa03ca223a0fd518dac2e6a7c590e60a6f1e11
Noteee <lugosi.norbert@gmail.com>
2017-06-13 09:52:14 +0200

    1-2 exercise done

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            var stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                var obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+                Console.ReadKey();
+            }
+
+            while (queue.Count > 0)
+            {
+                var obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+                Console.ReadKey();
+            }
         }
     }
 }
