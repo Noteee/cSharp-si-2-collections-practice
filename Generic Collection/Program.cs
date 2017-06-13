db31a91abdada2d8b47b5e65739dd9d7a035b735
Noteee <lugosi.norbert@gmail.com>
2017-06-13 10:41:30 +0200

    GenericCollection added

--- Generic Collection/Program.cs
+++ Generic Collection/Program.cs
@@ -10,6 +10,22 @@ namespace Generic_Collection
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> countryLookup = new Dictionary<int, string>();
+
+            countryLookup[44] = "United Kingdom";
+            countryLookup[33] = "France";
+            countryLookup[31] = "Netherlands";
+            countryLookup[55] = "Brazil";
+
+            Console.WriteLine("The 33 Code is for: {0}", countryLookup[33]);
+
+            foreach (var item in countryLookup)
+            {
+                int code = item.Key;
+                string country = item.Value;
+                Console.WriteLine("Code {0} = {1}", code, country);
+                Console.ReadKey();
+            }
         }
     }
 }
